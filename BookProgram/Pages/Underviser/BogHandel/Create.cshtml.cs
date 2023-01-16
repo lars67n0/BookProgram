8fa9f1fcbd095bda832f1621396529c58c013648
lars67n0 <lars67n0@edu.zealand.dk>
2023-01-16 13:05:37 +0100

    Monday All Pages Added

---
+++ BookProgram/Pages/Underviser/BogHandel/Create.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using BookProgram.Data;
+using BookProgram.Model;
+
+namespace BookProgram.Pages.Underviser.BogHandel
+{
+    public class CreateModel : PageModel
+    {
+        private readonly BookProgram.Data.BookProgramContext _context;
+
+        public CreateModel(BookProgram.Data.BookProgramContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+        ViewData["BookListID"] = new SelectList(_context.BookList, "ID", "Author");
+        ViewData["EducationID"] = new SelectList(_context.Education, "ID", "EducationName");
+            return Page();
+        }
+
+        [BindProperty]
+        public Orders Orders { get; set; }
+        
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+          
+
+            _context.Orders.Add(Orders);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ BookProgram/Pages/Underviser/BogHandel/Delete.cshtml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using BookProgram.Data;
+using BookProgram.Model;
+
+namespace BookProgram.Pages.Underviser.BogHandel
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly BookProgram.Data.BookProgramContext _context;
+
+        public DeleteModel(BookProgram.Data.BookProgramContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+      public Orders Orders { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Orders == null)
+            {
+                return NotFound();
+            }
+
+            var orders = await _context.Orders.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (orders == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Orders = orders;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null || _context.Orders == null)
+            {
+                return NotFound();
+            }
+            var orders = await _context.Orders.FindAsync(id);
+
+            if (orders != null)
+            {
+                Orders = orders;
+                _context.Orders.Remove(Orders);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
